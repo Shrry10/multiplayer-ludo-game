aac962e95f2cd8d2bdc18024737d92b4938929f7
Shrry10 <paridayash520@gmail.com>
2025-01-03 23:00:38 +0530

    Remove files now ignored by .gitignore

--- db.js
+++ db.js
@@ -1,16 +1,15 @@
-const { Pool } = require('pg');
-const dotenv = require('dotenv');
-const { connectionString } = require('pg/lib/defaults');
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const pool = new Pool({
-  // user: process.env.DB_USER,
-  // host: process.env.DB_HOST,
-  // database: process.env.DB_DATABASE,
-  // password: process.env.DB_PASSWORD,
-  // port: process.env.DB_PORT,
-  connectionString:process.env.DATABASE_URL
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  // connectionString:process.env.DATABASE_URL
 });
 
 module.exports = pool;
